05d0de0b9f722ed50c9d8d6b112a9a34bfffaeff
shxiaj <923066712@qq.com>
2022-10-28 20:23:47 +0800

    更新psoem脚本, 添加dipoles的计算

--- bashScript/psoem.sh
+++ bashScript/psoem.sh
@@ -3,33 +3,36 @@
 surfaceFile=sam_cooh.gro
 proteinFile=protein.gro
 
-dist2Bottom=0.05
-distPro2Surf=0.25
-
-surfTopArgs=("SAM  372"   "SBM  28")
+surfTopArgs=("SAM  238"   "SBM  18")
 surfName="SAM SBM"
+dipoleGroup="Protein"
 
 postiveCharges=0
-negativeCharges=32
+negativeCharges=10
+
+dist2Bottom=0.05
+distPro2Surf=0.25
+distpro2Top=1.78
 
 X0=$1
 X1=$2
 X2=$3
 X3=$4
 X4=$5
+X5=$6
 
 wd=$PWD
 
-if [ ! -d "$wd/part/p$6/" ]; then
-  mkdir -p $wd/part/p$6/
-  cp -rf $wd/model/* $wd/part/p$6/
+if [ ! -d "$wd/part/p$7/" ]; then
+  mkdir -p $wd/part/p$7/
+  cp -rf $wd/model/* $wd/part/p$7/
 else
-  rm $wd/part/p$6/\#*\#
-  rm $wd/part/p$6/*/\#*\#
-  cp -f $wd/model/topol.top $wd/part/p$6/
+  rm $wd/part/p$7/\#*\#
+  rm $wd/part/p$7/*/\#*\#
+  cp -f $wd/model/topol.top $wd/part/p$7/
 fi
 
-cd $wd/part/p$6/
+cd $wd/part/p$7/
 ########################################################################
 
 # $1 输入文件名; $2 输出文件名; $3 移动后到底边距离(nm)
@@ -70,11 +73,12 @@ function g_boxSize {
 # $1 组名; $2 gro文件名
 function g_findIndex {
   echo q | gmx make_ndx -f $2 -o 2>/dev/null |
+  sed -n '/atoms$/p' |
   awk -v resName="$1" '
-    BEGIN{ FIELDWIDTHS = "3 1 20"; split(resName, s);} 
+    BEGIN {split(resName, s);}
     { 
       for (i in s) {
-        if ($3 ~ s[i]) { idx[s[i]]=$1;}
+        if ($2 == s[i]) { idx[s[i]]=$1;}
       }
     } END {for (i in idx) {print idx[i]}}'
 }
@@ -115,58 +119,75 @@ function g_mer {
 
 # type gmx5 >/dev/null 2>&1 || { echo >&2 "Gromacs5.0 version not exist! Use System defalut version"; alias gmx5="gmx"; }
 
-# surface preparation
-
+### 1. surface preparation
+{
 g_adjPlus $surfaceFile surf.gro $dist2Bottom
-
 surfBoxThickness=`g_height surf.gro $dist2Bottom`
-
 surfBoxEdges=($(g_boxSize $surfaceFile))
+} > $7.log 2>&1
 
-
-
-# protein preparation
-
+### 2. protein preparation
+{
 gmx editconf -f $proteinFile -o ptw1-1.gro -rotate $X0 $X1 $X2
-
-proBoxThickness=`g_height ptw1-1.gro 1.78`
-
+proBoxThickness=`g_height ptw1-1.gro $distpro2Top`
 gmx editconf -f ptw1-1.gro -o ptw1-2.gro -box ${surfBoxEdges[0]} ${surfBoxEdges[1]} $proBoxThickness
-
 gmx editconf -f ptw1-2.gro -o ptw1-3.gro -translate $X3 $X4 0
-
+distPro2Surf=`echo ${distPro2Surf} ${X5} | awk '{print $1+$2}'`
 g_adjPlus ptw1-3.gro ptw2.gro $distPro2Surf
-
 gmx solvate -cp ptw2.gro -cs spc216.gro -o ptw3.gro -p topol.top
+} >> $7.log 2>&1
 
-# box Setting
-
-gmx editconf -f ptw3.gro -o ptw4.gro -translate 0 0 $surfBoxThickness
-
+### 3. box Setting
+gmx editconf -f ptw3.gro -o ptw4.gro -translate 0 0 $surfBoxThickness >> $7.log 2>&1
 g_mer surf.gro ptw4.gro
 
 
+### 4. writer2top
 for (( i = 0; i < ${#surfTopArgs[@]}; i++ )); do
   echo -e ${surfTopArgs[$i]} >> topol.top
 done
 
 
-# ions and em
-
+### 5. ions and em
+{
 mkdir ion em
-gmx grompp -f ion.mdp -c box.gro -p topol.top -o ./ion/ions.tpr -po ./ion/ionout.mdp -maxwarn 1
+gmx grompp -f ion.mdp -c box.gro -p topol.top -o ./ion/ions.tpr -po ./ion/ionout.mdp -maxwarn 2
 g_findIndex SOL box.gro | gmx genion -s ./ion/ions.tpr -o ions.gro -p topol.top -np $negativeCharges -pname NA -pq 1 -nn $postiveCharges -nname CL -nq -1
 g_boxzPlus ions.gro 0.05
+} >> $7.log 2>&1
 
+### 6. make_ndx for surface
+{
 surfId=($(g_findIndex "$surfName" ions.gro))
+
 idxSelect=`for i in ${surfId[@]}; do printf "%d | " $i;done`
 idxSelect=${idxSelect::-2}
+
 echo -e "$idxSelect \n q" | gmx make_ndx -f ions.gro -o
 lastId=$(($(grep -e "\[.*\]" index.ndx | wc -l) - 1))
 echo -e "name $lastId Surface\n q" | gmx make_ndx -f ions.gro -o -n
+} >> $7.log 2>&1
 
-gmxem
+### 7. runem
+gmxem >> $7.log 2>&1
 
-echo -e "`getEneGroup`\n\n" | gmx energy -f ./em/em.edr -sum
 
+### 8. get Protein-Surface
+echo -e "`getEneGroup`\n\n" | gmx energy -f ./em/em.edr -sum >> $7.log 2>&1
 awk 'END{print $2}' energy.xvg > ene.dat
+
+
+### 9. make_ndx for dipoles
+{
+dipId=($(g_findIndex "$dipoleGroup" ./em/em.tpr))
+idxSelect=`for i in ${dipId[@]}; do printf "%d | " $i;done`
+idxSelect=${idxSelect::-2}
+
+echo -e "$idxSelect \n q" | gmx make_ndx -f ./em/em.tpr -o
+
+lastId=$(($(grep -e "\[.*\]" index.ndx | wc -l) - 1))
+echo $lastId | gmx dipoles -f ./em/em.trr -s ./em/em.tpr -n
+
+# 修改了awk只输出一个cos
+awk '!/^(#|@)/ {theta = $4 / $5; print theta}' Mtot.xvg > dipoles.dat
+} >> $7.log 2>&1
